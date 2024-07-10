4c4656c682e426ae74f64b8c12f4a723e30beb3c
Hoang Nam Nguyen <HoangNam.Nguyen01@edu.sait.ca>
2024-07-09 21:53:12 -0600

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function openTab(event, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByTagName("a");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+  }
+  
+  // Default to open the first tab
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('home').style.display = 'block';
+  });
+  
